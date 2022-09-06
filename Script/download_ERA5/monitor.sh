4ea639b6833b75d7ca18a33b96cdda650932cb70
Handsome Jin <90272479+KingLLee@users.noreply.github.com>
2022-09-06 15:32:28 +0800

    Create monitor.sh

---
+++ Script/download_ERA5/monitor.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# This is the monitor script to monitor the era data download.
+
+year_begin=1979
+year_end=1985
+
+echo "Open up the monitor"
+
+# If you should download the data to the other end time, you can change the "2020" to another one.
+
+while [[ ${year_end} -le 2020 ]];
+
+do
+
+  # Call the shell "down_era5.sh"
+	bash down_era5.sh ${year_begin}  ${year_end}
+	
+
+	echo "sleep 40 min, keep running"
+	
+	# You can use the 'sleep' to adjust the period "year_begin - year_end" download time.
+  sleep 40m  
+	
+	echo "${year_begin}-${year_end} finished!!!!!"
+	
+
+	# Set the length of the period
+ 
+	year_begin=$((${year_begin}+7))
+ 
+	year_end=$((${year_end}+7))
+
+done
+
+
+
+echo "Work has been done!!!"
